d23fca0c979fc4510970132b69cfb08f60bc148d
Vishnu <pvishnuprasaath@gmail.com>
2017-09-10 13:04:34 +0530

    Server created

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const CryptoJS = require("crypto-js")
+const routes=require('./routes/api')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use('/api',routes)
+//app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+    res.send(' Welcome to BucketCrypt '+ ciphertext) 
+})
+
+app.listen(process.env.port||3000, function () {
+  console.log('Server started at port 3000')
+})
---
+++ server/routes/api.js
@@ -0,0 +1,83 @@
+const express=require('express')
+const bodyParser = require('body-parser')
+const CryptoJS = require("crypto-js")
+const rsa=require('../rsafun')
+const router=express.Router()
+
+
+var admin = require("firebase-admin");
+var serviceAccount = require("../public/cert/serviceAccountKey.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://bucketcrypt.firebaseio.com"
+});
+var db = admin.database()
+var ref = db.ref();
+var dataRef = ref.child("secretdata");
+
+
+router.post('/rsa/enc',function(req,res){
+    if (!req.body) return res.sendStatus(400)
+    var cipherText=rsa.encryptStringWithRsaPublicKey(req.body.text,'./public/cert/publicKey.PEM')
+    console.log(cipherText.toString())
+    dataRef.push({
+        text: req.body.text,
+        cipher: cipherText.toString()
+      });
+    res.send({
+        Text: req.body.text,
+        Cipher: cipherText.toString()
+    });
+});
+
+router.get('/rsa/dec',function(req,res){
+    var result={};
+    var key="final";
+    result[key]=[];
+    dataRef.on("value", function(snapshot) {
+        var data = snapshot.val();
+        console.log(data);
+        for(el in data){
+            data[el].cipher=rsa.decryptStringWithRsaPrivateKey(data[el].cipher,'./public/cert/privateKey.PEM')
+           result[key].push(data[el]); 
+        }
+        console.log(JSON.stringify(result));
+        res.send(JSON.stringify(result));
+      });
+    // var plainText=rsa.decryptStringWithRsaPrivateKey(req.body.cipher,'./cert/privateKey.PEM')
+    // console.log(plainText)    
+    
+});
+
+router.post('/rsa/dec',function(req,res){
+    if (!req.body) return res.sendStatus(400)
+    var plainText=rsa.decryptStringWithRsaPrivateKey(req.body.cipher,'./public/cert/privateKey.PEM')
+    console.log(plainText)    
+    res.send({
+        Cipher: req.body.cipher,
+        Text: plainText
+    });
+});
+
+router.post('/aes/enc', function (req, res) {
+    if (!req.body) return res.sendStatus(400)
+    var cipherText = CryptoJS.AES.encrypt(req.body.text, 'secret')
+    console.log(cipherText.toString())
+    res.send({
+        Text: req.body.text,
+        Cipher: cipherText.toString()
+    });
+  })
+  
+router.post('/aes/dec', function (req, res) {
+    if (!req.body) return res.sendStatus(400)
+    var bytes  = CryptoJS.AES.decrypt(req.body.cipher, 'secret')
+    var plainText = bytes.toString(CryptoJS.enc.Utf8)
+    console.log(plainText)    
+    res.send({
+        Cipher: req.body.cipher,
+        Text: plainText
+    });
+})
+
+module.exports=router
